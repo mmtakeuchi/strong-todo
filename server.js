2ec18548f09fc702c0f05ae77341a0318cfb039f
mmtakeuchi <mikemtaki@gmail.com>
2022-06-25 17:44:22 -0700

    update production code

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ mongoose
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
-app.get('/', function (request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
+}
 
 app.use('/api', todoRoutes);
 
