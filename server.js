b7cf198aa08b6fb060dc1bdbddfe8b2a84432f60
mmtakeuchi <mikemtaki@gmail.com>
2022-06-25 17:31:26 -0700

    add procfile and client build function in server.js

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ mongoose
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.get('/', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.use('/api', todoRoutes);
