7d1798c92ca1bb52c1ebab260ecb78ab648680c7
mmtakeuchi <mikemtaki@gmail.com>
2022-06-25 18:37:10 -0700

    change static build

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, './client/build')));
+  app.use(express.static('./client/build'));
   app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
   });
