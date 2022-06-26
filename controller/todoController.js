359e0b6adb7d402ed84f0763b4e9cd82584fb540
mmtakeuchi <mikemtaki@gmail.com>
2022-06-25 18:59:25 -0700

    remove procfile

--- controller/todoController.js
+++ controller/todoController.js
@@ -8,7 +8,6 @@ module.exports.getTodos = async (req, res) => {
     if (!todos) {
       res.send('Error getting todos.');
     }
-    console.log(todos);
 
     res.json(todos);
   } catch (error) {
